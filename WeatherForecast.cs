76d8e66cd751f0188c590b8423d75dfd20869b8c
hofy2 <mk7636302@gmail.com>
2022-02-25 23:04:19 +0200

    gg

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -13,6 +13,7 @@ namespace HotelBooking
         public string Summary { get; set; }
         public string et8yrWlaLa { get; set; }
         public string h5555555555555 { get; set; }
+        Console.WriteLine("kosom 7yate");
 
     }
 }
