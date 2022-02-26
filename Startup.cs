3d5714e13a50f12875f81c2835823c409a6fe319
Omar Sameh <omar.sameh.said2428@outlook.com>
2022-02-26 11:30:48 +0200

    db

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using HotelBooking.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,7 @@ namespace HotelBooking
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelBooking", Version = "v1" });
             });
+            services.AddDbContextPool<hotelsprojectContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("ProjectDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
