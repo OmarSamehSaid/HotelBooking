e815a4fec2446f989ac30766f7188e309cddeb77
Omar Sameh <omar.sameh.said2428@outlook.com>
2022-02-28 13:37:41 +0200

    userRep

--- bl/Repository/UserRep.cs
+++ bl/Repository/UserRep.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HotelBooking.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelBooking.bl.Repository
 {
@@ -16,8 +17,31 @@ namespace HotelBooking.bl.Repository
 
         public IEnumerable<User> Get()
         {
-            var data = DB.Users.Select(a => a);
+            var data = DB.Users.Where(a => a.Isdeleted != false).Select(a => a);
             return data;
+        }
+
+        public void Add(User user)
+        {
+            DB.Users.Add(user);
+            DB.SaveChanges();
+        }
+
+        private User GetUserById(int Id)
+        {
+            return DB.Users.Where(a => a.Userid == Id).FirstOrDefault();
+        }
+
+        public User GetById(int Id)
+        {
+            User data = GetUserById(Id);
+            return data;
+        }
+
+        public void Update(User user)
+        {
+            DB.Entry(user).State = EntityState.Modified;
+            DB.SaveChanges();
         }
 
     }
