600fbcb025a9606cd82561006ea4d1296610863a
Omar Sameh <omar.sameh.said2428@outlook.com>
2022-03-15 17:58:14 +0200

    hostt

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,17 +14,13 @@ namespace HotelBooking
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-          
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>()
-                .UseIISIntegration()
-                .UseKestrel().UseContentRoot(Directory.GetCurrentDirectory()).UseUrls("http://localhost:5000/", "http://192.168.0.243:5000/%22").UseIISIntegration().UseStartup<Startup>();
-            });
-
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
